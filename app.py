c4d461021dca1b32d154e383b2a1b326cad2acbb
akritii-3107 <107861050+akritii-3107@users.noreply.github.com>
2022-07-24 20:18:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 string = "Find if the number is even or odd"
-st.set_page_config(page_title=None, page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None,favicon=":random:")
+st.set_page_config(page_title=string, page_icon=":random:", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
 st.title('Find Odd or Even ')
 x = st.number_input('Enter a number')
