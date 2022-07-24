edeec96dff4fc31637bb22ce025db5bef84ea62e
akritii-3107 <107861050+akritii-3107@users.noreply.github.com>
2022-07-24 20:09:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 string = "Find if the number is even or odd"
 st.set_page_config(page_title=string)
 
-st.title('Find Odd-Even ')
+st.title('Find Odd or Even ')
 x = st.number_input('Enter a number')
 if (x).is_integer():
     if x%2==0:
-      st.header("the number is even")
+      st.caption("the number is even")
     elif x%2!=0:
-      st.header("the number is odd")
+      st.caption("the number is odd")
 else:st.subheader("please enter an integer")
